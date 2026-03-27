0ebd7212743afae035dc43bacdd7b3396d6f9888
Sharkbii2k <toquangtri500@gmail.com>
2026-03-27 11:43:55 +0700

    Set up Firebase configuration and services
    Added Firebase configuration and initialization.

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,17 @@
-export const firebaseConfig = null;
+// Firebase SDK
+const firebaseConfig = {
+  apiKey: "AIzaSyAOmMwSmTCpaDsMbXp2Z_LHISRhZVI2fKI",
+  authDomain: "hoanguhsk-82b5f.firebaseapp.com",
+  projectId: "hoanguhsk-82b5f",
+  storageBucket: "hoanguhsk-82b5f.firebasestorage.app",
+  messagingSenderId: "89093597870",
+  appId: "1:89093597870:web:69b435509128776e5a1b3f",
+  measurementId: "G-RGBY73JHC4"
+};
+
+// Init Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Services
+const auth = firebase.auth();
+const db = firebase.firestore();
